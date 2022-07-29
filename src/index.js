06896a56786e51dfe55049d53c5f119116e15dd2
Tadynas <griciustadas@gmail.com>
2022-07-29 14:25:06 +0300

    test hashing

--- src/index.js
+++ src/index.js
@@ -13,3 +13,20 @@ app.use(userRouter)
 app.listen(port, () => {
     console.log(`Server is up on port: ${port}`)
 })
+
+const bcrypt = require('bcryptjs')
+
+const myFunction = async () => {
+    const password = 'password'
+
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    console.log(password)
+    console.log(hashedPassword)
+
+    const isMatch = await bcrypt.compare('Password', hashedPassword)
+
+    console.log(isMatch)
+}
+
+myFunction()
