df32119c9964c63a2a508d17f140efb282b6da16
Tadynas <griciustadas@gmail.com>
2022-08-02 15:17:50 +0300

    clean-up index.js

--- src/index.js
+++ src/index.js
@@ -15,18 +15,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log(`Server is up on port: ${port}`)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('62e90c0c0e124f6083a13c28')
-    // await task.populate('owner')
-    // console.log(task.owner)
-
-    const user = await User.findById('62e909630e124f6083a13c23')
-    await user.populate({ path: 'tasks', strictPopulate: false })
-    console.log(user.tasks)
-}
-
-main()
