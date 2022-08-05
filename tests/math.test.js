7f40369c941f5a488c168f27d0d5d9dd6b3fadc5
Tadynas <griciustadas@gmail.com>
2022-08-05 14:50:21 +0300

    async testing

--- tests/math.test.js
+++ tests/math.test.js
@@ -9,3 +9,13 @@ test('Should calculate total with default tip', () => {
     const total = calculateTip(10)
     expect(total).toBe(12.5)
 })
+
+test('Async test demo', (done) => {
+    setTimeout(() => {
+        try {
+            expect(1).toBe(1)
+        } finally {
+            done()
+        }
+    }, 2000);
+})
